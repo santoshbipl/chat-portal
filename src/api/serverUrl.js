ce6c0a7b977828c5bd0dc47160e243a59cc19e8a
santoshbipl <166791008+santoshbipl@users.noreply.github.com>
2025-03-24 19:19:18 +0530

    Update serverUrl.js

--- src/api/serverUrl.js
+++ src/api/serverUrl.js
@@ -1,6 +1,9 @@
 const ServerUrl = {
+  BASE_URL: "https://atlshopuk.pythonanywhere.com",
+  WS_BASE_URL: "wss://atlshopuk.pythonanywhere.com/"
+};
+const ServerUrlold = {
   BASE_URL: "http://atlshopuk.pythonanywhere.com",
   WS_BASE_URL: "ws://atlshopuk.pythonanywhere.com/"
 };
-
 export default ServerUrl;
