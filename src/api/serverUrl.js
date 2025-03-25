f7c83e48386b5bc775eca1bcbe873de027646760
santoshbipl <166791008+santoshbipl@users.noreply.github.com>
2025-03-25 11:25:12 +0530

    Update serverUrl.js

--- src/api/serverUrl.js
+++ src/api/serverUrl.js
@@ -1,6 +1,6 @@
 const ServerUrl = {
-  BASE_URL: "https://atlshopuk.pythonanywhere.com",
-  WS_BASE_URL: "wss://atlshopuk.pythonanywhere.com"
+  BASE_URL: "https://atlshopuk.pythonanywhere.com/",
+  WS_BASE_URL: "wss://atlshopuk.pythonanywhere.com/"
 };
 const ServerUrlold = {
   BASE_URL: "http://atlshopuk.pythonanywhere.com",
